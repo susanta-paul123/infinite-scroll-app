4cb1c8f4fd34e91ec6ca0a1acc759deca5aa80ab
Susanta <desigs5500@gmail.com>
2020-10-17 12:04:59 +0530

    build app

---
+++ build/precache-manifest.b050dd78211c3bf82fa7762bc35d3918.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0671e15f8929a902f1b8dc7f4d6581ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23b1c7d586ed685c8151",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "8ece9d24f7bc840961aa",
+    "url": "/static/css/main.6e3fc27d.chunk.css"
+  },
+  {
+    "revision": "23b1c7d586ed685c8151",
+    "url": "/static/js/2.b647bd2e.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.b647bd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ece9d24f7bc840961aa",
+    "url": "/static/js/main.56b2a361.chunk.js"
+  },
+  {
+    "revision": "267cc3bea4704d8afe65",
+    "url": "/static/js/runtime-main.407b2b46.js"
+  },
+  {
+    "revision": "8d2917604632a49425aef9e4e18bb5e2",
+    "url": "/static/media/winters.8d291760.jpg"
+  }
+]);
